942f0bd2c0b53ac57e0f67e5d11731390cd1ffa9
Maksim Filippov <maksimphilippov@yahoo.com>
2023-06-20 19:07:42 +0300

    temporary test data for component writing

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import { NutritionalValue } from '../../types/NutritionalValue'
 
 export default function Home() {
+  const [mealList, setMealList] = React.useState<[NutritionalValue]>(
+    [
+      {
+        name: "rice",
+        mass: 100,
+        calories: 130,
+      },
+    ]
+  )
   return (
     <div>Home</div>
   )
