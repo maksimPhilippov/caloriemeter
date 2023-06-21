1ae71d54159acbe39b8548d277b6acd8360daff8
Maksim Filippov <maksimphilippov@yahoo.com>
2023-06-21 16:46:23 +0400

    home and meal works via CustomEvent

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,20 +1,65 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { NutritionalValue } from "../../types/NutritionalValue";
 import Meal from "../Meal/Meal";
+import MealFinder from "../MealFinder/MealFinder";
 
 export default function Home() {
-  const [mealList, setMealList] = React.useState<[NutritionalValue]>([
+  const [mealList, setMealList] = React.useState<NutritionalValue[]>([
     {
       name: "rice",
       mass: 100,
       caloriesCoeficient: 1.3,
     },
   ]);
+
+  const thisElement = useRef<HTMLDivElement>(null);
+  const setEventListener = useEffect(() => {
+    const ChangeMassListener = (e: Event) => {
+      if (isCustomEvent(e)) {
+        changeMassEventListener(e);
+      }
+    };
+    thisElement.current?.addEventListener("massChange", ChangeMassListener);
+    return () => {
+      thisElement.current?.removeEventListener(
+        "massChange",
+        ChangeMassListener
+      );
+    };
+  });
+
+  function isCustomEvent(e: Event): e is CustomEvent {
+    return "detail" in e;
+  }
+
+  function changeMassEventListener(event: CustomEvent) {
+    let newMealList = mealList.slice(0);
+    newMealList[event.detail.index].mass = event.detail.newvalue;
+    setMealList(newMealList);
+  }
+
+  function addNewMeals(addMeals: NutritionalValue[]) {
+    let newMeals = addMeals.reduce((proved: NutritionalValue[], meal) => {
+      if (mealList.find((element) => element.name == meal.name) == undefined) {
+        // no such food in mealList
+        proved.push(meal);
+      }
+      return proved;
+    }, []);
+
+    if (newMeals.length != 0) {
+      let newMealList = mealList.copyWithin(-1, 0);
+      newMealList.concat(newMeals);
+      setMealList(newMealList);
+    }
+  }
+
   return (
-    <div>
-      {mealList.map((mealItem) => (
-        <Meal item={mealItem} />
+    <div ref={thisElement}>
+      {mealList.map((mealItem, index) => (
+        <Meal key={index} item={mealItem} index={index} />
       ))}
+      <MealFinder />
     </div>
   );
 }
--- src/components/Meal/Meal.tsx
+++ src/components/Meal/Meal.tsx
@@ -3,28 +3,38 @@ import { NutritionalValue } from "../../types/NutritionalValue";
 
 interface MealProp {
   item: NutritionalValue;
+  index: number;
 }
 
 export default function Meal(prop: MealProp) {
-  const [changingMass, setChangingMass] = React.useState(prop.item.mass);
+  const thisElement = React.useRef<HTMLDivElement>(null);
 
   function validateMass(mass: string): void {
     let value = parseInt(mass);
     if (Number.isNaN(value)) {
       value = 0;
     }
-    setChangingMass(value);
+
+    let massChange = new CustomEvent("massChange", {
+      bubbles: true,
+      detail: {
+        index: prop.index,
+        newvalue: value,
+      },
+    });
+
+    thisElement.current?.dispatchEvent(massChange);
   }
 
   return (
-    <div className="Meal">
+    <div ref={thisElement} className="Meal">
       <p>{prop.item.name}</p>
       <input
         type="text"
-        value={changingMass}
+        value={prop.item.mass}
         onChange={(e) => validateMass(e.target.value)}
       />
-      <p>{prop.item.caloriesCoeficient * prop.item.mass}</p>
+      <p>{(prop.item.caloriesCoeficient * prop.item.mass).toFixed(0)}</p>
     </div>
   );
 }
